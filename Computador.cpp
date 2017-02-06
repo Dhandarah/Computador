3330eb6e460e8d34e592d46967590340b2439de9
Dhandarah <dhandarah1996@gmail.com>
2017-02-06 18:09:29 -0300

    Update Computador.cpp

--- Computador.cpp
+++ Computador.cpp
@@ -10,7 +10,15 @@ Computador::Computador()
 {
     this->nomeLogin = "desconhecido";
     this->senhaLogin = "****";
-    this->ptr=0;
+    this->ptr=0
+    this->memoPrincipal = 0;
+    this->memoSecundaria = 0;
+    this->nome_SO = 0;
+    this->plRede = 0;
+    this->plSom = 0;
+    this->plVideo = 0;
+    this->nome_SO = "Desconhecido";
+    
 }
  Computador::Computador (const string &nomeLogin,const string &senhaLogin)
  {
@@ -32,7 +40,7 @@ Computador::Computador()
 
  }
 
-void memoria(const int &mP, const int &mS)
+void exibirMemoria(const int &mP, const int &mS)
 {
   memoPrincipal = mP;
   memoSecundaria = mS;
@@ -42,7 +50,7 @@ void memoria(const int &mP, const int &mS)
   "Memoria Secundaria" <<memoSecundaria;
 }
 
-int dispositivos_IO()
+int exibirDispositivos_IO()
 {
   plVideo = 512;
   plSom = 16 ;
@@ -54,10 +62,17 @@ int dispositivos_IO()
 }
 
 
- ostream &operator<<( ostream &output, const Computador &informacoes)
+ ostream &operator<<( ostream &output, const Computador &info)
  {
-     output<<"O tamanho da memoria princial e da memoria secundaria "<<informacoes.memoPrincipal
-           "--" <<informacoes.memoSecundaria;
+     output<<"Informações: "
+        "Memoria Principal: "<<info.memoPrincipal
+           "\n" 
+           "Memoria Secundaria: "<<info.memoSecundaria<<
+           "\n"
+           "Placa de Video: "<<info.plVideo<< "\n" 
+           "Placa de Rede: "<<info.plRede<<
+           "\n"
+           "Placa de Som: "<<info.plSom;
  }
 
  const Computador& Computador::operator=(const Computador &c)
@@ -68,7 +83,7 @@ int dispositivos_IO()
    this->plRede = c.plRede;
    this->plSom = c.plSom;
    this->nome_SO = c.nome_SO;
-   delete [] nome_SO;
+   
 
    this->nome_SO = new nome_SO[c.tamMonitor]; 
 
